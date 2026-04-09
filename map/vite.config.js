d323678e1b38135ff7bf4fbcf40babcd24cf4d5f
darigakuandykova43@gmail.com <darigakuandykova43@gmail.com>
2026-04-09 17:37:14 +0600

    fix base path

--- map/vite.config.js
+++ map/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/street-art-map/', // ← ВОТ ЭТО ДОБАВЬ
+  base: '/street-art-map/',
 })
